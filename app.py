29a46117ac197cdb015b75c93ffaeda583876621
Jonatan Beristain <berisberis@gmail.com>
2019-05-12 14:17:49 -0700

    Constructors in for inputs Classes and arguments when instantiating
    To make project more object oriented. 
    Also updated some methods to class methods.

--- app.py
+++ app.py
@@ -7,33 +7,42 @@ from helpers.files import CsvMake
 
 class IChing:
 
-    def run(self, iterations, sets):
-        all_the_sets = []
-        one_set = self.create_set(iterations, 'w')
-        all_the_sets.append(one_set)
-        for r in range(sets-1):
-            one_set = self.create_set(iterations, 'a')
-            all_the_sets.append(one_set)
-        return all_the_sets
+    def __init__(self, iterations, sets):
+        self.iterations = iterations
+        self.sets = sets
 
-    def create_set(self, iterations, mode):
-        make_csv = CsvMake()
+    all_the_sets = []
+
+    def run(self):
+        one_set = self.create_set('w')
+        self.all_the_sets.append(one_set)
+        for r in range(self.sets-1):
+            one_set = self.create_set('a')
+            self.all_the_sets.append(one_set)
+        return self.all_the_sets
+
+    def create_set(self, mode):
         iteration = Iteration()
-        counted_iteration = iteration.iterate(iterations)
-        this_set = make_csv.sets_to_csv(mode, self.sort_the_sets(Results.print_results(counted_iteration)))
+        counted_iteration = iteration.iterate(self.iterations)
+        results = Results(counted_iteration)
+        results.print_results()
+        sorted_sets = self.sort_the_sets(counted_iteration)
+        make_csv = CsvMake()
+        this_set = make_csv.sets_to_csv(mode, sorted_sets)
         return this_set
 
     @staticmethod
     def k_sort(d):
         return {k: d[k] for k in sorted(d.keys())}
 
-    def sort_the_sets(self, the_sets):
+    @classmethod
+    def sort_the_sets(cls, the_sets):
         the_hex_set, the_tri_set, the_bin_set = the_sets
-        k_sorted_hex_set = self.k_sort(the_hex_set)
-        k_sorted_tri_set = self.k_sort(the_tri_set)
-        k_sorted_bin_set = self.k_sort(the_bin_set)
+        k_sorted_hex_set = cls.k_sort(the_hex_set)
+        k_sorted_tri_set = cls.k_sort(the_tri_set)
+        k_sorted_bin_set = cls.k_sort(the_bin_set)
         return k_sorted_hex_set, k_sorted_tri_set, k_sorted_bin_set
 
 
-iching = IChing()
-iching.run(iterations=4096, sets=8)
+iching = IChing(iterations=4096, sets=8)
+iching.run()

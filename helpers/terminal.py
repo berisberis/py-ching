a1991985b78c3ae5f78d6ef3be25b0261736a5d5
Jonatan Beristain <berisberis@gmail.com>
2019-05-12 20:25:50 -0700

    Updated Results class

--- helpers/terminal.py
+++ helpers/terminal.py
@@ -8,20 +8,22 @@ class Results:
 
     def print_results(self):
         counted_hex, counted_tri, counted_bin = self.counted_sets
-        (hex_first_place, hex_first_times), (hex_second_place, hex_second_times) = counted_hex.most_common(2)
-        (tri_first_place, tri_first_times), (tri_second_place, tri_second_times) = counted_tri.most_common(2)
-        (bin_first_place, bin_first_times), (bin_second_place, bin_second_times) = counted_bin.most_common(2)
+        (hex_first_place, hex_first_count), (hex_second_place, hex_second_count) = counted_hex.most_common(2)
+        (tri_first_place, tri_first_count), (tri_second_place, tri_second_count) = counted_tri.most_common(2)
+        (bin_first_place, bin_first_count), (bin_second_place, bin_second_count) = counted_bin.most_common(2)
 
-        print(f'\nHexagram {finder.transform_king_wen(hex_first_place)} was found {hex_first_times} times')
+        print('-'*30)
+        print(f'\nHexagram {finder.transform_king_wen(hex_first_place)} was found {hex_first_count} times')
         print(finder.find_king_wen_meaning(finder.transform_king_wen(hex_first_place)))
-        print(f'\nHexagram {finder.transform_king_wen(hex_second_place)} was found {hex_second_times} times')
+        print(f'\nHexagram {finder.transform_king_wen(hex_second_place)} was found {hex_second_count} times')
         print(finder.find_king_wen_meaning(finder.transform_king_wen(hex_second_place)))
 
-        print(f'\nTrigram {tri_first_place} was found {tri_first_times} times')
+        print(f'\nTrigram {tri_first_place} was found {tri_first_count} times')
         print(finder.find_trigram_meaning(tri_first_place))
-        print(f'\nTrigram {tri_second_place} was found {tri_second_times} times')
+        print(f'\nTrigram {tri_second_place} was found {tri_second_count} times')
         print(finder.find_trigram_meaning(tri_second_place))
 
-        print(f'\nBinary {bin_first_place} was found {bin_first_times} times')
-        print(f'Binary {bin_second_place} was found {bin_second_times} times')
+        print(f'\nBinary {bin_first_place} was found {bin_first_count} times')
+        print(f'Binary {bin_second_place} was found {bin_second_count} times')
+        print('-'*30)
 

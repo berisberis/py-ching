4ee8bc99b6e26f34a49d4f61ffca82a8ebd86aab
Jonatan Beristain <berisberis@gmail.com>
2019-05-15 13:05:59 -0700

    Removed range limits in charts

--- iching/flask_web.py
+++ iching/flask_web.py
@@ -12,14 +12,14 @@ def web_root(iterations, sets):
 
     hex_values = list(results[0][0].values())
     hex_keys = list(results[0][0].keys())
-    hex_chart = pygal.Line(range=(45, 90))
+    hex_chart = pygal.Line()
     hex_chart.x_labels = hex_keys
     hex_chart.add('Hexagrams', hex_values)
     hex_chart = hex_chart.render_data_uri()
 
     tri_values = list(results[0][1].values())
     tri_keys = list(results[0][1].keys())
-    tri_chart = pygal.Line(range=(900, 1100))
+    tri_chart = pygal.Line()
     tri_chart.x_labels = tri_keys
     tri_chart.add('Trigrams', tri_values)
     tri_chart = tri_chart.render_data_uri()
